24b0e2f698adf36426eb1f63ed54af405115a261
hellold9 <hellold9@126.com>
2026-02-13 15:05:26 +0800

    创建 videos.js

---
+++ videos.js
@@ -0,0 +1,16 @@
+const videos = [
+  {
+    id: "video1",
+    title: "丹尼尔版晴天",
+    file: "video1.mp4",
+    cover: "cover/cover1.jpg",
+    desc: "你猜他为什么叫晴天？因为周杰伦都无雨了。 #鬼畜 #雷霆 #低音炮"
+  },
+  {
+    id: "video2",
+    title: "示例视频 2",
+    file: "video2.mp4",
+    cover: "cover/cover2.jpg",
+    desc: "这是另一个示例简介，可以自定义内容。"
+  }
+];
