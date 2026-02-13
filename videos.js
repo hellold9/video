746fc2b37931ba873ac89c78ad31e3f6a346cc27
hellold9 <hellold9@126.com>
2026-02-13 15:34:07 +0800

    更新 videos.js

--- videos.js
+++ videos.js
@@ -7,6 +7,13 @@ const videos = [
     desc: "你猜他为什么叫晴天？因为周杰伦都无雨了。 #鬼畜 #雷霆 #低音炮"
   },
   {
+    id: "video3",
+    title: "基～米～到",
+    file: "video3.mp4",
+    cover: "cover/cover3.jpg",
+    desc: "新年听基米，越听越有米😝"
+  },
+  {
     id: "video2",
     title: "【春晚鬼畜】赵本山：我就是念诗之王！【改革春风吹满地】",
     file: "video2.mp4",
